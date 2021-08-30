0b711421a62518bf663109f559ca1e09a357d29b
Edwin Veltman <edwinator@gmail.com>
2021-08-30 14:53:00 -0700

    radix sort

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -9,14 +9,46 @@
 
 */
 
+function getDigit(num, place, longest) {
+  const string = num.toString();
+  const size = string.length;
+
+  const mod = longest - size;
+  return string[place - mod] || 0;
+}
+
+function getLongestNumLength(arr) {
+  let res = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const len = arr[i].toString().length;
+    if (len > res) res = len;
+  }
+  return res;
+}
+
 function radixSort(array) {
-  // code goes here
+  const longestNumLen = getLongestNumLength(array);
+  const buckets = new Array(10).fill().map(() => []);
+
+  for (let i = longestNumLen - 1; i >= 0; i--) {
+    while (array.length) {
+      const curr = array.shift();
+      buckets[getDigit(curr, i, longestNumLen)].push(curr);
+    }
+    for (let j = 0; j < buckets.length; j++) {
+      while (buckets[j].length) {
+        array.push(buckets[j].shift());
+      }
+    }
+  }
+
+  return array;
 }
 
 // unit tests
 // do not modify the below code
 test("radix sort", function () {
-  test.skip("should sort correctly", () => {
+  test("should sort correctly", () => {
     const nums = [
       20,
       51,
@@ -65,7 +97,7 @@ test("radix sort", function () {
       3001
     ]);
   });
-  test.skip("should sort 99 random numbers correctly", () => {
+  test("should sort 99 random numbers correctly", () => {
     const fill = 99;
     const nums = new Array(fill)
       .fill()
